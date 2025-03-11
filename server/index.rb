35605c9e3429fd117ab2562cf8a8d18d855f9664
selfhost <6418006+conchyliculture@users.noreply.github.com>
2025-03-11 13:39:24 +0100

    cleanup

--- server/index.rb
+++ server/index.rb
@@ -1,13 +1,10 @@
 #!/usr/bin/ruby
-# encoding: utf-8
 
 Encoding.default_external = Encoding::UTF_8
 
 require 'digest'
 require 'json'
 require 'sinatra'
-
-$config_file = File.join(File.expand_path(File.dirname(__FILE__)), 'config.json')
 
 def dohash(pwd)
   hex = pwd.strip()
@@ -17,18 +14,12 @@ def dohash(pwd)
   return hex
 end
 
-def load_config(path, default={})
-  config = default
+def load_config(path, default = {})
+  config = default.dup
   if File.exist?(path)
-    config = JSON.load(File.read(path))
+    config = JSON.parse(File.read(path))
   end
   return config
-end
-
-def save_config(path, data)
-  File.open(path, 'w') do |f|
-    f.write(data.to_json())
-  end
 end
 
 def get_password()
@@ -38,57 +29,64 @@ def get_password()
 end
 
 def json_msg(msg)
-    content_type 'application/json', :charset => 'utf-8'
-    return {
-      'status'=> 'ok',
-      'msg'=> msg
-    }.to_json
+  content_type 'application/json', charset: 'utf-8'
+  return {
+    'status' => 'ok',
+    'msg' => msg
+  }.to_json
 end
 
 def json_error(error)
-    content_type 'application/json', :charset => 'utf-8'
-    return {
-      'status'=> 'error',
-      'msg'=> error
-    }.to_json
+  content_type 'application/json', charset: 'utf-8'
+  return {
+    'status' => 'error',
+    'msg' => error
+  }.to_json
 end
 
-def gen_filename(f)
-  ext = File.extname(f)
+def gen_filename(filename, data)
+  generated_name = nil
+  ext = File.extname(filename)
   ext = 'jpg' if ext == ''
-  base = File.basename(f, ext)
-  dest = base
-  begin
-    dest = Digest::MD5.hexdigest(dest)[7..14]
-  end while File.exist?(File.join($settings['pics_dir'], dest+ext))
-  return dest+ext
+
+  md5 = Digest::MD5.hexdigest(data)
+  loop do
+    hash = md5[7..14]
+    generated_name = hash + ext
+    break unless File.exist?(generated_name)
+
+    md5 = Digest::MD5.hexdigest(md5)
+  end
+
+  return generated_name
 end
 
-$settings = load_config(
-  $config_file,
-  {
-    'my_url' => 'http://localhost:4567',
-    'secret' => '',
-    'pics_dir' => 'pics'
-  }
-)
+CONFIG_DEFAULTS = {
+  'my_url' => 'http://localhost:4567',
+  'secret' => '',
+  'pics_dir' => 'pics'
+}.freeze
 
-if $settings['secret'] == ''
-  $settings['secret'] = get_password()
-  save_config($config_file, $settings)
-  exit
+configure do
+  puts "ran config"
+  config_file = File.join(File.expand_path(__dir__), 'config.json')
+  options = load_config(config_file, CONFIG_DEFAULTS)
+  if options['secret'] == ''
+    options['secret'] = get_password()
+    File.open(config_file, 'w') do |f|
+      f.write(options.to_json())
+    end
+  end
+
+  Dir.mkdir(options['pics_dir']) unless File.directory?(options['pics_dir'])
+  pp options
+
+  set :options, options
 end
-
-if not File.directory?($settings['pics_dir'])
-  Dir.mkdir($settings['pics_dir'])
-end
-
 
 get '/p/:pic' do
-  if not params[:pic]
-    halt 404
-  end
-  path = File.join($settings['pics_dir'], params[:pic])
+  halt 404 unless params[:pic]
+  path = File.join(settings.options['pics_dir'], params[:pic])
   if File.exist?(path)
     send_file(path)
   else
@@ -98,7 +96,7 @@ end
 
 post '/upload' do
   if params[:secret]
-    if params[:secret] != $settings['secret']
+    if params[:secret] != settings.options['secret']
       halt 403, 'bad password'
     end
   else
@@ -107,12 +105,13 @@ post '/upload' do
   if params[:file]
     filename = params[:file][:filename]
     file = params[:file][:tempfile]
+    data = file.read()
 
-    dest_file = gen_filename(filename)
-    File.open(File.join($settings['pics_dir'], dest_file), 'wb') do |f|
-      f.write file.read
+    dest_file = gen_filename(filename, data)
+    File.open(File.join(settings.options['pics_dir'], dest_file), 'wb') do |f|
+      f.write data
     end
-    json_msg $settings['my_url'] + '/p/' + dest_file
+    json_msg "#{settings.options['my_url']}/p/#{dest_file}"
   else
     json_error 'You have to choose a file'
   end
